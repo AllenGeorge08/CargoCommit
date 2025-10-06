c5b8230679a4d8a395ac973af574c2c56b2b4967
Allen <allengeorge2400@gmail.com>
2025-10-06 19:50:17 +0000

    Added first commit

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,80 @@
 use names::Generator;
 use std::process::{Command, exit};
 
-
-//e Add git remote add origin, change branch everything..
-fn update_commit_push() {
-
-    //git init
-    //git remote -v
-    //git remote add origin if doesn't exist
-    //git 
-
-    //git add .
-    let add_command = Command::new("git")
-        .arg("add")
-        .arg(".")
+fn git_init() {
+    let init_command = Command::new("git")
+        .arg("init")
         .output()
-        .expect("Failed to execute git add command");
+        .expect("Failed to execute git init command");
 
-    if !add_command.status.success() {
-        println!("Error: Failed to add files to the git repo");
+    if !init_command.status.success() {
+        println!("Error: failed to init the repo");
         exit(1);
     }
-
-    //git commit -m "Message"
-    let commit_command = Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg(name_generator())
-        .output()
-        .expect("Failed to commit");
-
-    if !commit_command.status.success() {
-        println!("Error: Failed to commit to the git repo");
-        exit(2);
-    }
-
-    //git pussh origin master....
-    let push_command = Command::new("git")
-        .arg("push")
-        .arg("origin")
-        .arg("master")
-        .output()
-        .expect("Failed to push");
-
-    if !push_command.status.success() {
-        println!("Error: Failed to push to the git remote repo");
-        exit(3);
-    }
-
-    println!("Succesfully addded,commited and pushed all changes");
 }
 
-fn name_generator() -> String{
+fn git_add_remote_origin(url: &str) {
+    let git_add_command = Command::new("git")
+        .args(["remote", "add", "origin", url])
+        .output()
+        .expect("Failed to execute git command");
+
+    if !git_add_command.status.success() {
+        println!("Error: failed to add to remote origin");
+    }
+}
+
+fn git_check_branch_and_change_branch() {
+    let git_check_branch = Command::new("git")
+        .args(["branch", "-v"])
+        .output()
+        .expect("Failed to execute git command");
+
+    if !git_check_branch.status.success() {
+        println!("Error: failed to check branch");
+        exit(3);
+    }
+}
+
+fn git_add_upstream_url(url: &str) {
+    let git_add_upstream_url = Command::new("git")
+        .args(["remote", "add", "upstream", url])
+        .status()
+        .expect("Failed to add upstream url");
+
+    if !git_add_upstream_url.success() {
+        println!("Error: Failed to add upstream url");
+        exit(4);
+    }
+}
+
+fn git_rebase() {}
+
+fn git_merge() {}
+
+fn git_pull() {}
+
+fn git_add(){
+
+}
+
+fn git_commit(){
+
+}
+
+fn git_push(){
+
+}
+
+
+
+//e Use this in commit
+fn name_generator() -> String {
     let mut generator = Generator::default();
     generator.next().unwrap()
 }
 
-fn main() {
-    update_commit_push();
-    println!("Welcome to the git automate CLI");
-}
+// fn main() {
+//     update_commit_push();
+//     println!("Welcome to the git automate CLI");
+// }
